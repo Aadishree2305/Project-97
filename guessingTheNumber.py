008692e2de93fda182316794a4cbe977b0e6f890
Aadishree2305 <71633621+Aadishree2305@users.noreply.github.com>
2021-06-15 18:07:09 +0530

    Add files via upload

---
+++ guessingTheNumber.py
@@ -0,0 +1,9 @@
+print()
+print("Guess the number between 1 to 10")
+pocketMoney= int(input("Guessing the number: "))
+if(pocketMoney>5):
+    print("High")
+elif(pocketMoney<5):
+    print("Low")
+else:
+    print("Correct!")
